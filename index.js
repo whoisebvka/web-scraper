5987433685fe60987c6a0fc9286342d70c194d32
whoisebvka <vebuzor@gmail.com>
2022-10-12 13:07:56 +0100

    basic scrapping logic

--- index.js
+++ index.js
@@ -1,9 +1,29 @@
 const PORT = 8000
 const axios = require("axios");
 const cheerio = require("cheerio");
-const { append } = require("domutils");
+const { response } = require("express");
 const express = require("express");
 
 const app = express();
 
-append.listen(PORT, () => console.log(`Server running on PORT : ${PORT}`));
+const url = 'https://www.theguardian.com/international'
+
+axios(url)
+    .then(response => {
+        const html = response.data
+        const $ = cheerio.load(html)
+        const articles = []
+
+        $('.fc-item__title', html).each(function() {
+            const title = $(this).text()
+            const url = $(this).find('a').attr('href')
+            articles.push({
+                title,
+                url
+            })
+
+        })
+        console.log(articles)
+    }).catch(err => console.log(err))
+
+app.listen(PORT, () => console.log(`Server running on PORT : ${PORT}`));
